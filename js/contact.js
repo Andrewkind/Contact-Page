5fdb8278a17bacfbbbec6acbf50cc97db2d066b2
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-26 15:26:47 -0600

    Transition to error list.

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,13 @@
 
 console.log("astart");
 
-var sendEmailButton = document.querySelector(".send-email-button");
-sendEmailButton.addEventListener("click", buttonClick);
+var sendsubjectButton = document.querySelector(".send-email-button");
+sendsubjectButton.addEventListener("click", buttonClick);
 
 function buttonClick(event) {
 
     /*
-    var alerts = document.getElementsByClassName('alert');
+    var alerts = document.getElementsByClassemail('alert');
     console.log(alerts);
     for (var i = 0, l = alerts.length; i < l; i++) {
         alerts[i].style.display = "block";
@@ -16,12 +16,12 @@ function buttonClick(event) {
     // Validate Data
 
     var form = document.getElementById("form");
-    let nameAlert = document.querySelector(".alert-name");
     let emailAlert = document.querySelector(".alert-email");
-    let messageAlert = document.querySelector(".alert-message");
-    let messageAlertText = document.querySelector(".alert-message-text");
-    let nameAlertText = document.querySelector(".alert-name-text");
+    let subjectAlert = document.querySelector(".alert-subject");
+    let bodyAlert = document.querySelector(".alert-body");
+    let bodyAlertText = document.querySelector(".alert-body-text");
     let emailAlertText = document.querySelector(".alert-email-text");
+    let subjectAlertText = document.querySelector(".alert-subject-text");
 
 
     // Variable to hold our form data's validity. Sets to false if we find any errors.
@@ -32,14 +32,14 @@ function buttonClick(event) {
     var formData = new FormData(form);
     console.log(form);
 
-    var name = formData.get("name").trim();
-    console.log(name);
-
     var email = formData.get("email").trim();
     console.log(email);
 
-    var message = formData.get("message").trim();
-    console.log(message);
+    var subject = formData.get("subject").trim();
+    console.log(subject);
+
+    var body = formData.get("body").trim();
+    console.log(body);
 
 
     // Clear all errors
@@ -51,61 +51,54 @@ function buttonClick(event) {
 
 
 
-    // Check Name Validity
-    if (isEmpty(name)) {
-        // name is empty
-        validData = false;
-        nameAlertText.innerHTML = "Please fill in the box";
-        nameAlert.classList.remove("hide");
-    }
-
-
-    if (!isClean(name)) {
-        validData = false;
-        nameAlertText.innerHTML = "Vulgar!";
-        nameAlert.classList.remove("hide");
-    }
-
-
-    // Check Email Validity
+    // Check email Validity
     if (isEmpty(email)) {
-        // name is empty
+        // email is empty
         validData = false;
         emailAlertText.innerHTML = "Please fill in the box";
         emailAlert.classList.remove("hide");
     }
-    if (!validateEmail(email)) {
-        // email is not a valid email address
-        validData = false;
-        emailAlertText.innerHTML = "Invalid email address!";
 
-        emailAlert.classList.remove("hide");
-    }
+
     if (!isClean(email)) {
         validData = false;
         emailAlertText.innerHTML = "Vulgar!";
         emailAlert.classList.remove("hide");
     }
 
-    // Check Body Validity
-    if (isEmpty(message)) {
-        // name is empty
-        validData = false;
-        messageAlertText.innerHTML = "Please fill in the box";
 
-        messageAlert.classList.remove("hide");
-    }
-    if (!isClean(message)) {
+    // Check subject Validity
+    if (isEmpty(subject)) {
+        // email is empty
         validData = false;
-        messageAlertText.innerHTML = "Vulgar!";
-        messageAlert.classList.remove("hide");
+        subjectAlertText.innerHTML = "Please fill in the box";
+        subjectAlert.classList.remove("hide");
+    }
+    if (!isClean(subject)) {
+        validData = false;
+        subjectAlertText.innerHTML = "Vulgar!";
+        subjectAlert.classList.remove("hide");
+    }
+
+    // Check Body Validity
+    if (isEmpty(body)) {
+        // email is empty
+        validData = false;
+        bodyAlertText.innerHTML = "Please fill in the box";
+
+        bodyAlert.classList.remove("hide");
+    }
+    if (!isClean(body)) {
+        validData = false;
+        bodyAlertText.innerHTML = "Vulgar!";
+        bodyAlert.classList.remove("hide");
     }
 
 
 
     if (!validData) {
 
-        //Notify user of email success
+        //Notify user of subject success
 
         // do button stuff
         event.preventDefault();
@@ -115,14 +108,6 @@ function buttonClick(event) {
 
 }
 
-
-// Validate an email 
-//https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-// This is out of requirements but nice to have. Regular expression to validate email address
-function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
 
 function isEmpty(text) {
 
